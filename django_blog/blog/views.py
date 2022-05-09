2eb4c548b8335888cc672359980d4ebf6887270a
johnmash43 <ellymash45@gmail.com>
2022-05-09 12:52:32 +0300

    added a new file and some content

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from  .models import Writer
 
 # Create your views here.
 def home(request):
+    user= Writer("Cynthia","Wangari")    
 
-    context = {}
+    context = {
+        'first_name':'John',
+        'last_name':'Mash',
+        "writer": user
+    }
     return render(request, "index.html", context)
+
+def blog(request):
+    context = {
+      
+    }
+    return render(request, "blog.html", context)
